f2ec557ca3b15bc0436a39ea40f7988ecf93a9f8
Meeze <kokor@gmx.de>
2019-11-25 20:14:25 +0100

    first

---
+++ src/java/de/realmeze/main/Main.java
@@ -0,0 +1,16 @@
+package de.realmeze.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+}
